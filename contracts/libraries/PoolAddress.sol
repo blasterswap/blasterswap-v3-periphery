c18d344b0e49f8379b018c7d1429504c12b9892d
lsblk07 <lsblk07@eth.io>
2024-05-02 20:47:43 +0700

    init pool code hash

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x6c7188604b726a9913dfd875ad1912491aa27916135d8bdd032756e16fdd4c22;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x11056fb142badfd47cb326d0b82cc1009163115a37f7a780a4a9df550dfd65d0;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
