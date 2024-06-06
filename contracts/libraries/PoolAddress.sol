1349bf99f9bc55d635430f735f81f844e0e9b5fe
lsblk07 <lsblk07@eth.io>
2024-06-06 22:16:09 +0800

    mainnet params

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x372ec0d523f4a6309c837341f2caa98a5d65eb22be98fda499a4790839b8124e;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0xb20a785107ae67cdf09b4600a8185f072ebc5c7bf45b9427a8ae8c843e6d984e;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
